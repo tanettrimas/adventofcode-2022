7afcbe108bb8236b58fc8163d41578829fc4f9f7
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-12-01 14:27:39 +0100

    Løsning for oppgave 1

--- app/src/main/kotlin/adventofcode2022/app/day1/Day1.kt
+++ app/src/main/kotlin/adventofcode2022/app/day1/Day1.kt
@@ -1,5 +1,35 @@
 package adventofcode2022.app.day1
 
+import java.io.File
+
 fun main() {
-    println("Day1 running")
+    println("Oppgave 1: ${Day1().task1()}")
+}
+
+class Day1 {
+    fun task1(): Int {
+        val urlToFile = this.javaClass.getResource("/day1/input.txt")
+        val file = File(urlToFile?.toURI() ?: throw IllegalStateException("No such file $urlToFile"))
+        val lines = file.useLines {
+            mapToElves(it)
+        }
+        val elves = Elves(lines)
+        val elfWithMostCalories = elves.mostCalories()
+        return elfWithMostCalories.totalCalories()
+    }
+
+    private fun mapToElves(it: Sequence<String>): List<Elf> {
+        var list = mutableListOf<Int>()
+        val elvesList = mutableListOf<Elf>()
+        for (s in it) {
+            if (s.isBlank()) {
+                elvesList.add(Elf(list))
+                list = mutableListOf()
+                continue
+            } else {
+                list.add(s.toInt())
+            }
+        }
+        return elvesList.toList()
+    }
 }

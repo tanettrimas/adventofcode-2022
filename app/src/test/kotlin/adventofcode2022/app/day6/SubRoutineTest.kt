f77f49277b83a19a60b281a1692b824ab8e1445e
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-12-09 13:01:54 +0100

    Skriv tester for task 2

--- app/src/test/kotlin/adventofcode2022/app/day6/SubRoutineTest.kt
+++ app/src/test/kotlin/adventofcode2022/app/day6/SubRoutineTest.kt
@@ -6,10 +6,20 @@ import kotlin.test.assertEquals
 class SubRoutineTest {
 
     @Test
-    fun startOfPacketMarker() {
+    fun startOfPacketMarkerWithDefault() {
         assertEquals(5, SubRoutine("bvwbjplbgvbhsrlpgdmjqwftvncz").startOfPacketMarker())
         assertEquals(6, SubRoutine("nppdvjthqldpwncqszvftbrmjlhg").startOfPacketMarker())
         assertEquals(10, SubRoutine("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").startOfPacketMarker())
         assertEquals(11, SubRoutine("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").startOfPacketMarker())
+    }
+
+    @Test
+    fun withSpecificSize() {
+        val distinctSizes = 14
+        assertEquals(19, SubRoutine("mjqjpqmgbljsphdztnvjfqwrcgsmlb", windowSize = distinctSizes).startOfPacketMarker())
+        assertEquals(23, SubRoutine("bvwbjplbgvbhsrlpgdmjqwftvncz", windowSize = distinctSizes).startOfPacketMarker())
+        assertEquals(23, SubRoutine("nppdvjthqldpwncqszvftbrmjlhg", windowSize = distinctSizes).startOfPacketMarker())
+        assertEquals(29, SubRoutine("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", windowSize = distinctSizes).startOfPacketMarker())
+        assertEquals(26, SubRoutine("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", windowSize = distinctSizes).startOfPacketMarker())
     }
 }

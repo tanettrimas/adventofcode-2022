2c7ae9410e688193d3bc2381dcd61c9bcb725b29
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-12-14 15:18:52 +0100

    Dag 8 - task 1
    Delvis implementasjon med edge test

--- app/src/main/kotlin/adventofcode2022/app/day8/Day8.kt
+++ app/src/main/kotlin/adventofcode2022/app/day8/Day8.kt
@@ -1,5 +1,36 @@
 package adventofcode2022.app.day8
 
 fun main() {
+}
 
+data class Position(val rowIndex: Int, val columnIndex: Int)
+
+class Tree(private val height: Int, private val position: Position, private val gridLength: Int) {
+    fun isOnEdge() =
+        (position.rowIndex == 0 || position.rowIndex == gridLength - 1) || (position.columnIndex == 0 || position.columnIndex == gridLength - 1)
+}
+
+class TreeStructure(input: String) {
+    companion object {
+        fun parseTreeStructure(input: String, lengthOfGrid: Int) = input
+            .split("\n")
+            .mapIndexed { rowIndex, line
+                ->
+                line
+                    .split("")
+                    .filter { it.isNotBlank() }
+                    .mapIndexed { columnIndex, s ->
+                        Tree(
+                            s.toInt(),
+                            Position(rowIndex, columnIndex),
+                            gridLength = lengthOfGrid
+                        )
+                    }
+            }
+    }
+
+    private val lengthOfGrid = input.split("\n").first().trim().length
+    private val trees: List<List<Tree>> = parseTreeStructure(input, lengthOfGrid)
+
+    fun visibleEdges() = trees.flatten().count(Tree::isOnEdge)
 }
---
+++ app/src/test/kotlin/adventofcode2022/app/day8/TreeStructureTest.kt
@@ -0,0 +1,20 @@
+package adventofcode2022.app.day8
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+
+internal class TreeStructureTest {
+    @Test
+    fun `gives correct edge trees`() {
+        val input = """
+            30373
+            25512
+            65332
+            33549
+            35390
+        """.trimMargin()
+        val treeStructure = TreeStructure(input)
+        assertEquals(16, treeStructure.visibleEdges())
+    }
+}

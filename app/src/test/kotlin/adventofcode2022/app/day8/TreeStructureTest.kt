5f5e4c86037cfa11fe58581afd59aa26cefff8bb
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-12-14 16:28:14 +0100

    Dag 8 - Implementasjon for interiøret av treet

--- app/src/test/kotlin/adventofcode2022/app/day8/TreeStructureTest.kt
+++ app/src/test/kotlin/adventofcode2022/app/day8/TreeStructureTest.kt
@@ -17,4 +17,17 @@ internal class TreeStructureTest {
         val treeStructure = TreeStructure(input)
         assertEquals(16, treeStructure.visibleEdges())
     }
+
+    @Test
+    fun `gives correct visible interior`() {
+        val input = """
+            30373
+            25512
+            65332
+            33549
+            35390
+        """.trimMargin()
+        val treeStructure = TreeStructure(input)
+        assertEquals(5, treeStructure.visibleInterior())
+    }
 }
